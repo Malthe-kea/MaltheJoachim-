f90d17194c67fe07ddbc157af6f230a7bea500e9
malthe-kea <mati0002@stud.kea.dk>
2024-03-04 09:35:16 +0100

    nej

--- src/MaltheogJoachim.java
+++ src/MaltheogJoachim.java
@@ -2,5 +2,6 @@ public class MaltheogJoachim {
     public static void main(String[] args) {
         System.out.println("hej");
         System.out.println("Hej igen");
+        System.out.println("jeg er uenig. ");
     }
 }
